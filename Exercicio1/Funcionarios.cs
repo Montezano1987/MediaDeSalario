5b39120dd28f12461da2703a4d2a72eedd4feec5
bruno <bruno@BRUNODESKTOP>
2024-05-10 08:12:12 -0300

    Add project files.

---
+++ Exercicio1/Funcionarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Course
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var culturaBrasileira = new CultureInfo("pt-BR");
+
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine(), new CultureInfo("pt-BR"));
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine(), new CultureInfo("pt-BR"));
+            double media = (f1.Salario + f2.Salario) / 2.0;
+            Console.WriteLine("Salário médio = " + media.ToString("F2", new CultureInfo("pt-BR")));
+        }
+    }
+}
